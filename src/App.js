dfb6d4af6ac97e8335a21e9cc55153f6fd521618
suhaimee24 <suhaimee_24@hotmail.com>
2020-06-15 14:25:09 +0700

    first submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Logo.png';
+import fuculty from './faculty.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo"/>
+        <div className="flex-column">
+          <div className="flex-1">แผนกงานวิชาการปริญญาตรี</div>
+          <div className="flex-2">คณะวิศวกรรมศาสตร์</div>
+        </div>
+        <img src={fuculty} className="fuculty-logo"/>
       </header>
+      <sidebar className="side-bar">
+        <item class ="bottom-sidebar">กลุ่มในสาขาวิชา</item>
+        <item class ="bottom-sidebar">วิชาในสาขาวิชา</item>
+        <item class ="bottom-sidebar">รายละเอียดวิชา</item>
+        <item class ="bottom-sidebar">ตารางสอน</item> 
+      </sidebar>
+      <main className="main">
+      main
+      </main>
+      <footer className="footer">
+      footer
+      </footer>
     </div>
   );
 }
